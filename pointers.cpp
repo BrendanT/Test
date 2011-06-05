68212f5b2b8f45967aa395ab96ffb9b353b0231f
BrendanT <brendan.tildesley@gmail.com>
2011-06-04 22:06:33 -0700

    Edited pointers.cpp via GitHub

--- pointers.cpp
+++ pointers.cpp
@@ -5,18 +5,15 @@ int main ()
 {
   int numbers[5];
   int * p;
-  p = numbers;  *p = 10;
-  cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
-  p++;  *p = 20;
-  cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
-  p = &numbers[2];  *p = 30;
-  cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
-  p = numbers + 3;  *p = 40;
-  cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
-  p = numbers;  *(p+4) = 50;
-  cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
+  p = numbers;  *p = 10;     cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
+  p++;  *p = 20;             cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
+  p = &numbers[2];  *p = 30; cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
+  p = numbers + 3;  *p = 40; cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
+  p = numbers;  *(p+4) = 50; cout << "numbers: " << numbers << ", p: " << p << ", *p: " << *p << endl;
+
   for (int n=0; n<5; n++)
     cout << numbers[n] << ", ";
   cout << endl;
+
   return 0;
 }
